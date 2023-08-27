6beae3a269292a115d822084578c8a7edee3a05f
Ilia Shapovalov <greateman@mail.ru>
2023-08-27 12:29:36 +0300

    initial commit

---
+++ BestRecipes/Extensions/UIColor+.swift
@@ -0,0 +1,40 @@
+//
+//  extension.swift
+//  CustomTabbar
+//
+//  Created by Максим Нурутдинов on 27.08.2023.
+//
+
+import Foundation
+import UIKit
+
+extension UIColor {
+    public convenience init?(hex: String, alpha: Double = 1.0) {
+        var pureString = hex.trimmingCharacters(in: .whitespacesAndNewlines).uppercased()
+        if (pureString.hasPrefix("#")) {
+            pureString.remove(at: pureString.startIndex)
+        }
+        if ((pureString.count) != 6) {
+            return nil
+        }
+        let scanner = Scanner(string: pureString)
+        var hexNumber: UInt64 = 0
+
+        if scanner.scanHexInt64(&hexNumber) {
+            self.init(
+                red: CGFloat((hexNumber & 0xFF0000) >> 16) / 255.0,
+                green: CGFloat((hexNumber & 0x00FF00) >> 8) / 255.0,
+                blue: CGFloat(hexNumber & 0x0000FF) / 255.0,
+                alpha: CGFloat(1.0))
+            return
+        }
+        return nil
+    }
+}
+
+extension UIColor {
+    
+    static var customBorderColor: UIColor { UIColor(red: 0.851, green: 0.851, blue: 0.851, alpha: 1) }
+}
+
+
---
+++ BestRecipes/Tabbar/MyCustomTabBarController.swift
@@ -0,0 +1,97 @@
+
+import UIKit
+import Foundation
+
+class MyCustomTabBarController : UITabBarController {
+    
+    let searchBar: UISearchBar = {
+        let searchBar = UISearchBar()
+        searchBar.sizeToFit()
+        searchBar.searchBarStyle = .minimal
+//        searchBar.searchTextField.font = UIFont(name: "Poppins-Regular", size: 10)
+//        searchBar.setImage(UIImage(named: "Union"), for: UISearchBar.Icon.search, state: .normal)
+        searchBar.searchTextField.borderStyle = .none
+//        searchBar.searchTextPositionAdjustment.horizontal = 10
+        searchBar.translatesAutoresizingMaskIntoConstraints = false
+//        searchBar.backgroundColor = .clear
+        searchBar.layer.cornerRadius = 16
+        searchBar.layer.borderWidth = 2
+        searchBar.layer.borderColor = UIColor.customBorderColor.cgColor
+        return searchBar
+    }()
+    
+    
+    
+    let btnMiddle : UIButton = {
+       let btn = UIButton(frame: CGRect(x: 0, y: 0, width: 50, height: 50))
+        btn.setTitle("", for: .normal)
+        btn.backgroundColor = UIColor(hex: "#fe989b", alpha: 1)
+        btn.layer.cornerRadius = 22
+        btn.layer.shadowColor = UIColor.black.cgColor
+        btn.layer.shadowOpacity = 0.2
+        btn.layer.shadowOffset = CGSize(width: 4, height: 4)
+        btn.setBackgroundImage(UIImage(named: "icons8-plus"), for: .normal) // картинка
+//        btn.setBackgroundImage(UIImage(systemName: "plus"), for: .normal) // системный плюс
+        return btn
+    }()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        navigationItem.titleView = searchBar
+        self.tabBar.addSubview(btnMiddle)
+        setupCustomTabBar()
+        addSomeTabItems()
+        btnMiddle.frame = CGRect(x: Int(self.tabBar.bounds.width)/2 - 22, y: -22, width: 44, height: 44)
+        
+    }
+
+    func setupCustomTabBar() {
+        let path : UIBezierPath = getPathForTabBar()
+        let shape = CAShapeLayer()
+        shape.path = path.cgPath
+        shape.lineWidth = 3
+        shape.strokeColor = UIColor.white.cgColor
+        shape.fillColor = UIColor.white.cgColor
+        self.tabBar.layer.insertSublayer(shape, at: 0)
+        self.tabBar.itemWidth = 40
+        self.tabBar.itemPositioning = .centered
+        self.tabBar.itemSpacing = 60
+        self.tabBar.tintColor = UIColor(hex: "#fe989b", alpha: 1.0)
+    }
+    
+    func addSomeTabItems() {
+        let vc1 = UINavigationController(rootViewController: ViewC1())
+        let vc2 = UINavigationController(rootViewController: ViewC2())
+        let vc3 = UINavigationController(rootViewController: ViewC3())
+        let vc4 = UINavigationController(rootViewController: ViewC4())
+        setViewControllers([vc1, vc2, vc3, vc4], animated: false)
+        guard let items = tabBar.items else { return}
+        items[0].image = UIImage(systemName: "house")
+        items[1].image = UIImage(systemName: "bookmark")
+        items[2].image = UIImage(systemName: "bell")
+        items[3].image = UIImage(systemName: "person")
+    }
+    
+    func getPathForTabBar() -> UIBezierPath {
+        let frameWidth = self.tabBar.bounds.width
+        let frameHeight = self.tabBar.bounds.height + 40
+        let holeWidth = 150
+        let holeHeight = 50
+        let leftXUntilHole = Int(frameWidth/2) - Int(holeWidth/2)
+        
+        let path : UIBezierPath = UIBezierPath()
+        path.move(to: CGPoint(x: 0, y: 0))
+        path.addLine(to: CGPoint(x: leftXUntilHole , y: 0)) // 1.Line
+        path.addCurve(to: CGPoint(x: leftXUntilHole + (holeWidth/3), y: holeHeight/2), controlPoint1: CGPoint(x: leftXUntilHole + ((holeWidth/3)/8)*6,y: 0), controlPoint2: CGPoint(x: leftXUntilHole + ((holeWidth/3)/8)*8, y: holeHeight/2)) // part I
+        
+        path.addCurve(to: CGPoint(x: leftXUntilHole + (2*holeWidth)/3, y: holeHeight/2), controlPoint1: CGPoint(x: leftXUntilHole + (holeWidth/3) + (holeWidth/3)/3*2/5, y: (holeHeight/2)*6/4), controlPoint2: CGPoint(x: leftXUntilHole + (holeWidth/3) + (holeWidth/3)/3*2 + (holeWidth/3)/3*3/5, y: (holeHeight/2)*6/4)) // part II
+        
+        path.addCurve(to: CGPoint(x: leftXUntilHole + holeWidth, y: 0), controlPoint1: CGPoint(x: leftXUntilHole + (2*holeWidth)/3,y: holeHeight/2), controlPoint2: CGPoint(x: leftXUntilHole + (2*holeWidth)/3 + (holeWidth/3)*2/8, y: 0)) // part III
+        path.addLine(to: CGPoint(x: frameWidth, y: 0)) // 2. Line
+        path.addLine(to: CGPoint(x: frameWidth, y: frameHeight)) // 3. Line
+        path.addLine(to: CGPoint(x: 0, y: frameHeight)) // 4. Line
+        path.addLine(to: CGPoint(x: 0, y: 0)) // 5. Line
+        path.close()
+        return path
+    }
+}
