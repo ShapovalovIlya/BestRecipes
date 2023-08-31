db6aec934c0882c1182f2999e648477c8bd83cef
Leonid <leo-turko@mail.ru>
2023-08-31 12:59:04 +0300

    Make DetailView Controller

---
+++ BestRecipes/Product.swift
@@ -0,0 +1,10 @@
+//
+//  Product.swift
+//  BestRecipes
+//
+//  Created by Леонид Турко on 30.08.2023.
+//
+
+struct Product: Hashable {
+  let name: String
+}
---
+++ BestRecipes/ProductList.swift
@@ -0,0 +1,48 @@
+//
+//  ProductList.swift
+//  BestRecipes
+//
+//  Created by Леонид Турко on 30.08.2023.
+//
+
+struct ProductList {
+  let featured = [
+    Product(name: "itemOne"),
+    Product(name: "itemTwo"),
+    Product(name: "itemThree"),
+    Product(name: "itemFour")
+  ]
+   
+   let onSale = [
+   Product(name: "1"),
+   Product(name: "2"),
+   Product(name: "3"),
+   Product(name: "4")
+   ]
+   
+   let all = [
+   Product(name: "one"),
+   Product(name: "two"),
+   Product(name: "three"),
+   Product(name: "four"),
+   Product(name: "five"),
+   Product(name: "six"),
+   Product(name: "seven"),
+   Product(name: "eight")
+   ]
+  /*
+  let onSale = [
+    Product(name: "itemOne"),
+    Product(name: "itemTwo"),
+    Product(name: "itemThree"),
+    Product(name: "itemFour")
+  ]
+  
+  let all = [
+    Product(name: "itemOne"),
+    Product(name: "itemTwo"),
+    Product(name: "itemThree"),
+    Product(name: "itemFour")
+  ]
+   */
+}
