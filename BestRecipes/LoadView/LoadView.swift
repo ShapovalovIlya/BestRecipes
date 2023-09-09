9bfe8f38e9b9bdb020a9a7072692ea891b9990fe
Leonid <leo-turko@mail.ru>
2023-09-09 21:58:47 +0300

    Make Load View

---
+++ BestRecipes/LoadView/LoadView.swift
@@ -0,0 +1,47 @@
+//
+//  LoadView.swift
+//  BestRecipes
+//
+//  Created by Леонид Турко on 09.09.2023.
+//
+
+import UIKit
+
+final class LoadingView: UIView {
+  private let activityIndicator = UIActivityIndicatorView(style: .large)
+  
+  override init(frame: CGRect) {
+    super.init(frame: frame)
+    setup()
+  }
+  
+  required init?(coder: NSCoder) {
+    fatalError("init(coder:) has not been implemented")
+  }
+}
+
+extension LoadingView {
+  private func setup() {
+    backgroundColor = UIColor(white: 0, alpha: 0.5)
+    
+    addSubview(activityIndicator)
+    activityIndicator.color = .white
+    activityIndicator.translatesAutoresizingMaskIntoConstraints = false
+    
+    NSLayoutConstraint.activate([
+      activityIndicator.centerXAnchor.constraint(equalTo: centerXAnchor),
+      activityIndicator.centerYAnchor.constraint(equalTo: centerYAnchor)
+    ])
+  }
+  
+  func startAnimating() {
+    activityIndicator.startAnimating()
+    isHidden = false
+  }
+  
+  func stopAnimating() {
+    activityIndicator.stopAnimating()
+    isHidden = true
+  }
+}
+
